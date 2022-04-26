e40726c67033a615f08a4650d3eb833883f9090e
get2721 <get2721@email.vccs.edu>
2022-04-26 17:27:13 -0400

    New objects created for Employee and Employee with Territory

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+        Employee employee1 = new Employee();
+
+        employee1.setId(1);
+        employee1.setSalary(50_000.00);
+        System.out.println("Here is Employee1: " + employee1.getId() + " "+ employee1.getSalary());
+
+
+        EmployeeWithTerritory employeeTerritory = new EmployeeWithTerritory();
+
+        employeeTerritory.setId(2);
+        employeeTerritory.setSalary(40_000.00);
+        employeeTerritory.setTerritory(50);
+
+        System.out.println("Here is employee with Territory: ID: " + employeeTerritory.getId() + " SALARY: "
+                + employeeTerritory.getSalary() + "Territory: " + employeeTerritory.getTerritory());
+
+
     }
 }
